869a5a9e94c3410b1c6c0eb8c6dae724cefa768a
Anthony Carreta <32708861+BackGrowZ@users.noreply.github.com>
2022-11-11 15:38:59 +0000

    update

--- app.js
+++ app.js
@@ -1,54 +1,18 @@
-// // import { Fetch } from "@octokit/types";
-// // import { Octokit } from "@octokit/core"
-// import axios from "axios";
-// import { createServer } from "http"
-
-// const generateURLFile = (repos,ext,epreuve) => {
-//     const urlList = [];
-//     const pseudoAndRepos = repos.split("https://github.com/")[1];
-//     let i = 0
-//     while (i<=15) {
-//         const nb = i<10 ? `0${i}` : i;
-//         let result = `https://raw.githubusercontent.com/${pseudoAndRepos}/main/${epreuve}${nb}.${ext}`;
-//         urlList.push(result);
-//         i++;
-//     }
-//     return urlList;
-// }
-
-// const test = async (req, res) => {   
-
-    
-//     axios.get('https://raw.githubusercontent.com/BackGrowZ/Pellencin/main/index.html', {
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     }).then((response) => {
-//         console.log(response.data);
-//         res.end(response.data);
-//     }).catch((err) => {
-//         console.log(err);
-//         res.end(err);
-//     });
-
-//     // const data = await response.json();
-//     // res.end(content);
-// }
-
-// createServer(test).listen(3000);
-
-import express from "express"
-import bodyParser from 'body-parser'
-import router from './router/router.js';
+import express from "express";
+import bodyParser from "body-parser";
+import router from "./router/router.js";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(express.static('public'))
+app.use("/static", express.static(__dirname + "/public"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-app.use('/', router)
+app.use("/", router);
 
 const PORT = 9300;
 
--- components/Class/Correction.js
+++ components/Class/Correction.js
@@ -1,144 +1,147 @@
-import spawn from 'await-spawn';
-import fs from 'fs';
-import terre from '../../data/terre.js';
+import spawn from "await-spawn";
+import fs from "fs";
+import terre from "../../data/terre.js";
 
 export default class Correction {
-    constructor(pseudo, epreuve, langage) {
-        this.pseudo = pseudo;
-        this.epreuve = epreuve;
-        this.langage = langage;
-        this.files = this.getFilesList();
-        this.data = this.generateDataTesting();
-    }
+  constructor(pseudo, epreuve, langage) {
+    this.pseudo = pseudo;
+    this.epreuve = epreuve;
+    this.langage = langage;
+    this.files = this.getFilesList();
+    this.data = this.generateDataTesting();
+  }
 
-    generateDataTesting() {
-        if(this.epreuve === "terre") {
-            const result = terre ;
-            result["Nom du programme"] = [{ args: [], result:`${this.pseudo}-${this.epreuve}01.${this.langage}` }];
-            return result;
+  generateDataTesting() {
+    if (this.epreuve === "terre") {
+      const result = terre;
+      result["Nom du programme"] = [{ args: [], result: `${this.pseudo}-${this.epreuve}01.${this.langage}` }];
+      return result;
+    } else {
+      const result = {};
+      return result;
+    }
+  }
+
+  async getResultFile(file, arg) {
+    let exec = "";
+    if (this.langage === "js") exec = "node";
+    else if (this.langage === "py") exec = "python";
+    else if (this.langage === "rb") exec = "ruby";
+    else if (this.langage === "php") exec = "php";
+
+    try {
+      const bl = await spawn(exec, [file, ...arg]);
+      return bl.toString().replace(/\r?\n|\r/g, " ");
+    } catch (e) {
+      // console.log(e.stderr.toString())
+    }
+  }
+
+  isMsgError(msg) {
+    const regex = /\berror|\berreur/gm;
+    const msgLower = msg.toLowerCase();
+    return msgLower.match(regex) !== null;
+  }
+
+  getFilesList() {
+    const result = [];
+    const NB_FILES = 16;
+    for (let i = 0; i <= NB_FILES; i++) {
+      if (i === NB_FILES) return result;
+      else {
+        const nb = i < 10 ? `0${i}` : i;
+        const name = `${this.pseudo}-${this.epreuve}${nb}.${this.langage}`;
+        result.push(name);
+      }
+    }
+  }
+
+  deleteFile() {
+    this.files.forEach((file) => {
+      fs.unlink(`files/${file}`, (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      });
+    });
+  }
+
+  outputInit(exercices) {
+    const result = {};
+    exercices.forEach((exercice) => {
+      result[exercice] = [];
+    });
+    return result;
+  }
+
+  // creer une classe qui recupere les sorties des fichiers qui se trouve dans this.files
+  async getOutput() {
+    const exercices = Object.keys(this.data);
+    const results = this.outputInit(exercices);
+    for (let nbExercice = 0; nbExercice < exercices.length; nbExercice++) {
+      const exercice = exercices[nbExercice];
+      const args = this.data[exercice];
+      for (let nbArgs = 0; nbArgs < args.length; nbArgs++) {
+        const arg = args[nbArgs].args;
+        const result = await this.getResultFile(`files/${this.files[nbExercice]}`, arg);
+        results[exercice].push(result);
+      }
+    }
+    return results;
+  }
+
+  generateShellPrompts(id, args) {
+    let shell = "";
+
+    if (this.langage === "js") shell = "node";
+    else if (this.langage === "py") shell = "python";
+    else if (this.langage === "rb") shell = "ruby";
+    else if (this.langage === "php") shell = "php";
+
+    shell += ` ${this.files[id]}`;
+
+    let shellArgs = "";
+    const max = args.length;
+    args.forEach((arg, i) => {
+      if (typeof arg === "string") {
+        if (i === max - 1) shellArgs += `"${arg}"`;
+        else shellArgs += `"${arg}", `;
+      } else {
+        if (i === max - 1) shellArgs += arg;
+        else shellArgs += `${arg}, `;
+      }
+    });
+    shell += ` ${shellArgs}`;
+    return shell;
+  }
+
+  async correction() {
+    const output = await this.getOutput();
+    const resultat = {};
+    const test = Object.keys(this.data);
+    for (const [key, value] of Object.entries(this.data)) {
+      resultat[key] = [];
+      for (let i = 0; i < value.length; i++) {
+        const { args, result, msg } = value[i];
+        const res = output[key][i];
+        const resIsValide = res !== undefined && res !== false;
+        const shell = this.generateShellPrompts(test.indexOf(key), args);
+        if (resIsValide && res.trim().toLowerCase().includes(result)) {
+          // Correct
+          resultat[key].push({ args, result: true, output: res, shell });
+        } else if (resIsValide && this.isMsgError(res)) {
+          // Error args
+          resultat[key].push({ args, result: true, output: res, msg, shell });
         } else {
-            const result = {};
-            return result;
-        } 
-    }
-
-    async getResultFile(file, arg) {
-        let exec = "";
-        if(this.langage === "js") exec = "node";
-        else if(this.langage === "py") exec = "python";
-        else if(this.langage === "rb") exec = "ruby";
-        else if(this.langage === "php") exec = "php";
-
-        try {
-            const bl = await spawn(exec, [file, ...arg]);
-            return bl.toString().replace(/\r?\n|\r/g, " ");
-        } catch (e) {
-            // console.log(e.stderr.toString())
+          // Mauvaise réponse
+          const message = msg ? msg : "Incorrect";
+          console.log({ args, result: false, output: res, msg: message, expected: result, shell });
+          resultat[key].push({ args, result: false, output: res, msg: message, expected: result, shell });
         }
+      }
     }
-
-    isMsgError(msg) {
-        const regex = /\berror|\berreur/gm;
-        const msgLower = msg.toLowerCase();
-        return msgLower.match(regex) !== null;
-    }
-
-    getFilesList() {
-        const result = []
-        const NB_FILES = 12;
-        for (let i = 0; i <= NB_FILES; i++) { 
-            if(i === NB_FILES) return result;
-            else {
-                const nb = i < 10 ? `0${i}` : i;
-                const name = `${this.pseudo}-${this.epreuve}${nb}.${this.langage}`;
-                result.push(name);
-            }
-        }
-    }
-
-    deleteFile() {
-        this.files.forEach((file) => {
-            fs.unlink(`files/${file}`, (err) => {
-                if (err) {
-                    console.error(err)
-                    return
-                }
-            })
-        })
-    }
-
-    outputInit(exercices) {
-        const result = {};
-        exercices.forEach((exercice) => {
-            result[exercice] = [];
-        })
-        return result;
-    }
-
-    // creer une classe qui recupere les sorties des fichiers qui se trouve dans this.files
-    async getOutput() {
-        const exercices = Object.keys(this.data);
-        const results = this.outputInit(exercices);
-        for (let nbExercice = 0; nbExercice < exercices.length; nbExercice++) {
-            const exercice = exercices[nbExercice];
-            const args = this.data[exercice];
-            for (let nbArgs = 0; nbArgs < args.length; nbArgs++) {
-                const arg = args[nbArgs].args;
-                const result = await this.getResultFile(`files/${this.files[nbExercice]}`,arg)
-                results[exercice].push(result);
-            }
-        }
-        return results;
-    }
-
-    generateShellPrompts(id, args) {
-        let shell = "";
-
-        if(this.langage === "js") shell = "node";
-        else if(this.langage === "py") shell = "python";
-        else if(this.langage === "rb") shell = "ruby";
-        else if(this.langage === "php") shell = "php";
-        
-        shell += ` ${this.files[id]}`;
-
-        let shellArgs = "";
-        const max = args.length;
-        args.forEach((arg,i) => {
-            if(typeof arg === "string") {
-                if(i === max - 1) shellArgs += `"${arg}"`;
-                else shellArgs += `"${arg}", `;
-            } else {
-                if(i === max - 1) shellArgs += arg;
-                else shellArgs += `${arg}, `;
-            }
-        })
-        shell += ` ${shellArgs}`;
-        return shell;
-    }
-
-    async correction() {
-        const output = await this.getOutput();
-        const resultat = {};
-        const test = Object.keys(this.data)
-        for (const [key, value] of Object.entries(this.data)) {
-            resultat[key] = [];
-            for (let i = 0; i < value.length; i++) {
-                const { args, result, msg } = value[i];
-                const res = output[key][i];
-                const resIsValide = res !== undefined && res !== false;
-                const shell = this.generateShellPrompts(test.indexOf(key), args);
-                if (resIsValide && res.trim().includes(result)) { // Correct
-                    resultat[key].push({ args, result: true, output: res, shell });
-                } else if (resIsValide && this.isMsgError(res)) { // Error args
-                    resultat[key].push({ args, result: true, output: res, msg, shell });
-                } else { // Mauvaise réponse
-                    const message = msg ? msg : "Incorrect";
-                    console.log({ args, result: false, output: res, msg:message, expected: result, shell })
-                    resultat[key].push({ args, result: false, output: res, msg:message, expected: result, shell });
-                }
-            }
-        }
-        this.deleteFile();
-        return resultat;
-    }
+    this.deleteFile();
+    return resultat;
+  }
 }
